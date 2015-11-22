ff9c3973f8c166c81dfa9b42a1c62a9104d5620f
DynData <tchadwick@dyn.com>
2015-11-22 12:50:19 -0500

    a data model with a supporting saturate function emerges.  I do not defend the coherence, or lack thereof.  <3

---
+++ data_test.go
@@ -0,0 +1,29 @@
+package PickemLib
+
+import (
+    "testing"
+)
+
+func TestData(t *testing.T) {
+    var pickemName = "Comp-698"
+    var d = Data(pickemName)
+    if d.Name != pickemName  {
+        t.Errorf("Error!  Data(%s) == %s, not %s", pickemName, d.Name, pickemName)
+    }
+}
+
+func TestSaturate(t *testing.T) {
+    var pickemName = "Week1"
+    var pickem = Data(pickemName)
+    saturate(pickem)
+    var one = 1
+    if pickem.PickemGames["first"].Weeks["one"] != one {
+        t.Errorf("Error!  pickem.Weeks[\"one\"] == %d, not %d", pickem.PickemGames["first"].Weeks["one"], one)
+    }
+    var wk1 = Week{ ID : "Week1"}
+    if pickem.PickemGames["first"].Players["Jon"].Picks[wk1.ID].Points != 5 {
+        t.Errorf("%s", pickem.PickemGames["first"].Players["Jon"].Picks[wk1.ID].Game.teamHome)
+        t.Errorf("%s", pickem.PickemGames["first"].Players["Jon"].Picks[wk1.ID].Game.teamAway)
+        t.Errorf("Error!  pickem.PickemGames[\"first\"].Players[\"Jon\"].Picks[wk1.ID].Points == %d, not %d", pickem.PickemGames["first"].Players["Jon"].Picks[wk1.ID].Points, 5)
+    }
+}
